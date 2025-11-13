6a7779cca1f6588b7673bc225b5802d04bb672fc
Ymiot <yanismiot64@gmail.com>
2025-11-13 16:28:39 +0100

    v3

--- lib/model/EncDecModel.py
+++ lib/model/EncDecModel.py
@@ -1,53 +1,47 @@
 import torch
 import torch.nn as nn
 
-class EncBlock(nn.Module):
-    def __init__(self, in_c, out_c):
-        super().__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(in_c, out_c, 3, padding=1),
-            nn.BatchNorm2d(out_c),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_c, out_c, 3, padding=1),
-            nn.BatchNorm2d(out_c),
-            nn.ReLU(inplace=True),
-        )
-    def forward(self, x):
-        return self.block(x)
-
-class DecBlock(nn.Module):
-    def __init__(self, in_c, out_c):
-        super().__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(in_c, out_c, 3, padding=1),
-            nn.BatchNorm2d(out_c),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_c, out_c, 3, padding=1),
-            nn.BatchNorm2d(out_c),
-            nn.ReLU(inplace=True),
-        )
-    def forward(self, x): 
-        return self.block(x)
-
+    
 class EncDec(nn.Module):
-    def __init__(self, in_channels=3, base=32):
+    def __init__(self):
         super().__init__()
-        self.enc1 = EncBlock(in_channels, base)
-        self.pool1 = nn.MaxPool2d(2)
-        self.enc2 = EncBlock(base, base*2)
-        self.pool2 = nn.MaxPool2d(2)
-        self.bottleneck = EncBlock(base*2, base*4)
-        self.up2 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
-        self.dec2 = DecBlock(base*4, base*2)
-        self.up1 = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
-        self.dec1 = DecBlock(base*2, base)
-        self.out = nn.Conv2d(base, 1, 1)
+
+        # encoder (downsampling)
+        self.enc_conv0 = nn.Conv2d(3, 64, 3, padding=1)
+        self.pool0 = nn.MaxPool2d(2, 2)  # 128 -> 64
+        self.enc_conv1 = nn.Conv2d(64, 64, 3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2)  # 64 -> 32
+        self.enc_conv2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2)  # 32 -> 16
+        self.enc_conv3 = nn.Conv2d(64, 64, 3, padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2)  # 16 -> 8
+
+        # bottleneck
+        self.bottleneck_conv = nn.Conv2d(64, 64, 3, padding=1)
+
+        # decoder (upsampling)
+        self.upsample0 = nn.Upsample(16)  # 8 -> 16
+        self.dec_conv0 = nn.Conv2d(64, 64, 3, padding=1)
+        self.upsample1 = nn.Upsample(32)  # 16 -> 32
+        self.dec_conv1 = nn.Conv2d(64, 64, 3, padding=1)
+        self.upsample2 = nn.Upsample(64)  # 32 -> 64
+        self.dec_conv2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.upsample3 = nn.Upsample(128)  # 64 -> 128
+        self.dec_conv3 = nn.Conv2d(64, 1, 3, padding=1)
+
     def forward(self, x):
-        x1 = self.enc1(x)
-        x2 = self.enc2(self.pool1(x1))
-        x3 = self.bottleneck(self.pool2(x2))
-        x = self.up2(x3)
-        x = self.dec2(x)
-        x = self.up1(x)
-        x = self.dec1(x)
-        return self.out(x)
+        # encoder
+        e0 = self.pool0(F.relu(self.enc_conv0(x)))
+        e1 = self.pool1(F.relu(self.enc_conv1(e0)))
+        e2 = self.pool2(F.relu(self.enc_conv2(e1)))
+        e3 = self.pool3(F.relu(self.enc_conv3(e2)))
+
+        # bottleneck
+        b = F.relu(self.bottleneck_conv(e3))
+
+        # decoder
+        d0 = F.relu(self.dec_conv0(self.upsample0(b)))
+        d1 = F.relu(self.dec_conv1(self.upsample1(d0)))
+        d2 = F.relu(self.dec_conv2(self.upsample2(d1)))
+        d3 = self.dec_conv3(self.upsample3(d2))  # no activation
+        return d3
